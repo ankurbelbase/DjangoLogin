e11d3d13afdfac50903411857e32f332a3b7901c
ankurbelbase <me.ankur49@gmail.com>
2017-03-20 14:01:24 +0545

    register

--- login/urls.py
+++ login/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
-
+from .views import *
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^register/$', register, name='register'),
+    url(r'^register/success/$', register_success),
 ]
