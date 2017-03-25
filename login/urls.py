604574144f57b95393574c31cb4b4b121e2f0b0d
ankurbelbase <me.ankur49@gmail.com>
2017-03-25 20:45:10 +0545

    register modified

--- login/urls.py
+++ login/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^register/$', register, name='register'),
-    url(r'^register/success/$', register_success),
+    
 ]
